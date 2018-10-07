2f30a598b3ebd3789d3ed0ca9f5feece630c2e2e
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2018-10-07 14:45:37 -0500

    Added/modified some stuff

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 var notificationRouter = require('./project/routing/notifications');
 var config = require('./config');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/notifications', notificationRouter);
 
-app.listen(config.PORT, () => console.log(`Notification API listening on port ${config.PORT}\nOpen http://localhost:3000/notifications to view`));
+app.listen(config.PORT, () => console.log(`Notification API listening on port ${config.PORT}\nOpen http://localhost:${config.PORT}/notifications to view`));
--- project/routing/notifications.js
+++ project/routing/notifications.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Notification Home Page");
+    res.send("Notification Home Page. This is cool");
 });
 
 module.exports = router;
