75a678aec5c419fa0f89edec31875d44ed584f70
m4-z3 <m4-z3@users.noreply.github.com>
2018-11-03 13:31:35 -0500

    added logout get route

--- project/routing/notifications.js
+++ project/routing/notifications.js
@@ -19,4 +19,17 @@ router.post('/', async function(req,res) {
     return res.send('Error');
 })
 
+router.get('/logout', function(req, res, next) {
+    if (req.session) {
+      // delete session object
+      req.session.destroy(function(err) {
+        if(err) {
+          return next(err);
+        } else {
+          return res.redirect('/');
+        }
+      });
+    }
+  });
+
 module.exports = router;
